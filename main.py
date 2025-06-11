6585f48e1455fc0dcb74eafc7d5cde6f57e769f2
whysurix <daniilcybrijj@gmail.com>
2025-06-11 13:38:11 +0300

    Исправлен

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from aiogram import Bot, Dispatcher, types, executor
-from database import init_db, save_data, get_data
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from config import BOT_TOKEN
+from handlers import client, admin, other
+from database.db import DataBase
 
-bot = Bot(token="YOUR_BOT_TOKEN")
-dp = Dispatcher(bot)
-db_conn = init_db()
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    
+    # Регистрация роутеров
+    dp.include_router(client.router)
+    dp.include_router(admin.router)
+    dp.include_router(other.router)
+    
+    # Инициализация базы данных
+    await DataBase.on_startup()
+    
+    # Запуск поллинга
+    await dp.run_polling(bot)
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    chat_id = message.chat.id
-    username = message.from_user.username
-    save_data(db_conn, chat_id, username, "Hello data")
-    data = get_data(db_conn, chat_id)
-    await message.reply(f"Welcome {username}! Your data: {data[1] if data else 'None'}")
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+    asyncio.run(main())
